4a5d163853e421dffe7445c57b7e46dc597a9938
Florian VIRAPIN KEMMA <florianvirapin.4@gmail.com>
2025-03-05 22:01:51 +0400

    configuration de routes avec Raect Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Dashboard from "./pages/Dashboard";
+import QuotationList from "./pages/QuotationList";
+import CustomerList from "./pages/CustomerList";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/quotation/list" element={<QuotationList />} />
+          <Route path="/customer/list" element={<CustomerList />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
